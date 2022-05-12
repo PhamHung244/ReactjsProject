3cd354aa184482eaf1fb6c8c5c01cd1dbf5ff1b0
PhamHung244 <phamngochung244@gmail.com>
2022-05-12 16:09:57 +0700

    hungupdate

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react'
 export default class index extends Component {
   render() {
     return (
-      <div>hung</div>
+      <div>hung
+        <div>update hung </div>
+      </div>
     )
   }
 }
